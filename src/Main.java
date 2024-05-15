9461af08a886dbb27f5b551dde15e0f0b358c17a
NoemiCg <161253351+NoemiCg@users.noreply.github.com>
2024-05-15 16:05:34 +0200

    vamos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-/**
- * 
- */
+	import java.util.Scanner;
 
-/**
- * 
- */
-public class Main {
+	public class Main {
 
-}
+		public  static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Ingrese su edad actual en años: ");
+			int edad = scanner.nextInt();
+		
+			Persona persona = new Persona(edad);
+			persona.calcularAñosHastaJubilacion();
+
+		}
+	}
+
